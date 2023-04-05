52b694e962a93dc4cb0508b29e75aa3e98310f3a
Shambhuraj27 <127391185+Shambhuraj27@users.noreply.github.com>
2023-04-05 19:40:49 +0530

    Add files via upload

---
+++ Head Brain Case Study/Head Brain size Predictor using Linear Regression.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+def HeadBrain():
+    data = pd.read_csv("HeadBrain .csv")
+    print("Size of Dataset",data.shape)
+
+    X = data["Head Size(cm^3)"].values
+    Y = data["Brain Weight(grams)"].values
+
+    X = X.reshape((-1,1))
+    n = len(X)
+
+    reg = LinearRegression()
+    reg = reg.fit(X,Y)
+    y_pred = reg.predict(X)
+    r2 = reg.score(X,Y)
+    print(r2)
+
+def main():
+    print("Head Brain Size Predictor using Linear regresion")
+    HeadBrain()
+if __name__ == "__main__":
+    main()
