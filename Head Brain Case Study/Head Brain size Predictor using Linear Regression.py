9788a11668a24421cbc94ca72c373788c4ebd374
Shambhuraj Patil <127391185+Shambhuraj27@users.noreply.github.com>
2024-06-13 13:48:17 +0530

    Head Brain size Predictor using Linear Regression.py

--- Head Brain Case Study/Head Brain size Predictor using Linear Regression.py
+++ Head Brain Case Study/Head Brain size Predictor using Linear Regression.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 def HeadBrain():
     data = pd.read_csv("HeadBrain .csv")
@@ -8,17 +9,24 @@ def HeadBrain():
 
     X = data["Head Size(cm^3)"].values
     Y = data["Brain Weight(grams)"].values
-    
+
+    X = X.reshape((-1,1))
+
     reg = LinearRegression()
-    
     reg = reg.fit(X,Y)
     y_pred = reg.predict(X)
-    
+
+    plt.scatter(X, Y, color="Orange")
+    plt.plot(X, y_pred, color="Green")
+    plt.xlabel("Head Size (cm^3)")
+    plt.ylabel("Brain Weight (grams)")
+    plt.show()
+
     r2 = reg.score(X,Y)
-    print(r2)
+    print("Accuracy of Linear Regression is :",r2*100)
 
 def main():
-    print("Head Brain Size Predictor using Linear regresion")
+    print("Head Brain Size Predictor using Linear regression")
     HeadBrain()
 if __name__ == "__main__":
     main()
