858524741a99bbc7aa1c9c315b40bfa98fb4cc83
Shambhuraj Patil <127391185+Shambhuraj27@users.noreply.github.com>
2024-07-27 01:16:12 +0530

    Play Predictor application using K Nearest Neighbor.py

--- Play Predictor Case Study/Play Predictor application using K Nearest Neighbor.py
+++ Play Predictor Case Study/Play Predictor application using K Nearest Neighbor.py
@@ -7,23 +7,23 @@ def PlayPredictor(data_path):
     data = pd.read_csv(data_path,index_col=0)
     print("Size of dataset :",len(data))
 
-    feature_names = ["Whether","Temperature"]
+    feature_names = ["Weather","Temperature"]
     print("Names of feature are ",feature_names)
 
-    Whether = data.Whether
+    Weather = data.Weather
     Temperature = data.Temperature
     Play = data.Play
 
     le = preprocessing.LabelEncoder()
 
-    Whether_encoder = le.fit_transform(Whether)
-    print(Whether_encoder)
+    Weather_encoder = le.fit_transform(Weather)
+    print(Weather_encoder)
 
     Temperature_encoder = le.fit_transform(Temperature)
     label = le.fit_transform(Play)
     print(Temperature_encoder)
 
-    features = list(zip(Whether_encoder,Temperature_encoder))
+    features = list(zip(Weather_encoder,Temperature_encoder))
 
     Model = KNeighborsClassifier()
 
