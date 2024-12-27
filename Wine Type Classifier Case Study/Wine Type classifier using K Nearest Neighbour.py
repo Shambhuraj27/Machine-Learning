4fd34c0f2f7ba4d5521a28037ae5696b220dc291
Shambhuraj Patil <127391185+Shambhuraj27@users.noreply.github.com>
2024-12-27 23:45:47 +0530

    Wine Type classifier using K Nearest Neighbour.py

--- Wine Type Classifier Case Study/Wine Type classifier using K Nearest Neighbour.py
+++ Wine Type Classifier Case Study/Wine Type classifier using K Nearest Neighbour.py
@@ -1,38 +1,57 @@
 
-from sklearn import metrics
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-def WinePredictor():
-    # Load Dataset
-    Wine = datasets.load_wine()
+def wine_predictor():
+    dataset = pd.read_csv("WinePredictor.csv")
+    print(dataset.head())
 
-    # print the names of features
-    print(Wine.feature_names)
+    x = dataset.drop("Class",axis=1)
+    y = dataset["Class"]
+    
+    # Feature scaling for scale the training features
+    sc = StandardScaler()
+    print(sc.fit(x))
+    print(sc.transform(x))
+    x = pd.DataFrame(sc.transform(x),columns=x.columns)
+    print(x.head())
+    
+    # Visualization 
+    print("Visualizing pairplot...")
+    sns.pairplot(dataset, hue='Class', diag_kind='kde', markers=["o", "s", "D"])
+    plt.title("Pairplot of Features")
+    plt.show()
+    
+    # Split the data into training and testing
+    xtrain,xtest,ytrain,ytest = train_test_split(x,y,test_size=0.3,random_state=42)
 
-    # print the label species(class_0,class_1,class_2)
-    print(Wine.target_names)
+    reg = LogisticRegression(max_iter=10000,multi_class="multinomial",class_weight="balanced")
+    
+    # Train the model
+    reg.fit(xtrain,ytrain)
+    
+    # Test the model
+    prediction = reg.predict(xtest)
+    
+    # Calculate accuracy
+    Accuracy = accuracy_score(ytest,prediction)
+    print("Accuracy using Logistic Regression :",Accuracy*100,"%")
 
-    # print the wine data(top 5 records)
-    print(Wine.target[0:5])
-
-    # print the wine labels(0:class_0,1:class_1,2:class_2)
-    print(Wine.target)
-
-    x_train,x_test,y_train,y_test = train_test_split(Wine.data,Wine.target,test_size=0.3) #70% for training and 30% for testing
-
-    # Create KNN classifir
-    knn = KNeighborsClassifier(n_neighbors=3)
-
-    knn.fit(x_train,y_train)
-    knn.predict(x_test)
-
-    print("Accuracy:",metrics.accuracy_score(y_test,knn.predict(x_test)))
+    # Custom input
+    custom_input =[[13.24,2.59,2.87,21.0,118,2.80,2.69,0.39,1.82,4.32,1.04,2.93,735]]
+    
+    # Scale custom input for prediction
+    custom_input_scaled = sc.transform(custom_input)
+    custom_prediction = reg.predict(custom_input_scaled)
+    print("Prediction for the provided custom input:", custom_prediction[0])
 
 def main():
-    print("Wine Predictor application using KNN algorithn")
-    WinePredictor()
+    print("Wine predictor")
+    wine_predictor()
 if __name__ == "__main__":
     main()
-
